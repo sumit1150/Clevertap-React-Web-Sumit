009855ac38e40589fa40f994e1659ed6a3bb5fcf
Sumit Kumar Sharma <36254393+sumit1150@users.noreply.github.com>
2022-03-31 18:44:04 +0530

    clevertap code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import clevertap from 'clevertap-web-sdk';
+
+
+// event without properties
+/*clevertap.event.push("Product viewed");
+
+// event with properties
+clevertap.event.push("Product viewed", {
+    "Product name": "Casio Chronograph Watch",
+    "Category": "Mens Accessories",
+    "Price": 59.99,
+    "Date": new Date()
+});*/
+
+
 
 function App() {
+
+  const pushCTEvent = () => {
+    clevertap.event.push("WebEvent", {
+      "Action":"Success",
+      "Category":"Shirts",
+      "Price":59.99,});
+      alert("WebEvent pushed to CT");
+  }
+
+   const pushCTEvent2 = () => {
+    clevertap.event.push("DemoWebEvent", {
+      "Action":"Success",
+      "Category":"Shirts",
+      "Price":59.99,});
+    alert("Event 2  pushed to CT");
+   }
+
+   const ctNotificationConsent= () => {
+    clevertap.notifications.push({
+      "titleText":'Would you like to receive Push Notifications?',
+      "bodyText":'We promise to only send you relevant content and give you updates on your transactions',
+      "okButtonText":'Sign me up!',
+      "rejectButtonText":'No thanks',
+      "okButtonColor":'#f28046'});
+    alert("Notification Consent pushed to CT");
+   }
+
+   const updateProfile = () => {
+    clevertap.profile.push({
+      "Site": {
+  
+      "tier": "Platinum",           
+  }
+})
+    alert("profile func pushed to CT");
+   }
+   const loginFunction = () => {
+    clevertap.onUserLogin.push({
+      "Site": {
+      "Name": "Sumit Sharma",            // String
+      "Identity": 115037,              // String or number
+      "Email": "sumitkumar782@gmail.com",         // Email address of the user
+      "Phone": "+919351210270",           // Phone (with the country code)
+      "Gender": "M",                     // Can be either M or F
+      "DOB": new Date(),                 // Date of Birth. Date object
+      // optional fields. controls whether the user will be sent email, push etc.
+      "MSG-email": true,                // Disable email notifications
+      "MSG-push": true,                  // Enable push notifications
+      "MSG-sms": false,                   // Enable sms notifications
+      "MSG-whatsapp": false,             // Enable WhatsApp notification
+  }
+})
+    alert("login func func pushed to CT");
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+<body>
+        <p>This page is having Clevertap SDK in-built</p>
+        <button onClick={loginFunction}>Login</button>
+        <button onClick={pushCTEvent}>Push WebEvent</button>
+        <button onClick={pushCTEvent2}>Push DemoWebEvent</button>
+        <button onClick={ctNotificationConsent}>WebPushConsent</button>
+        <button onClick={updateProfile}>profile Update</button>
+    </body>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import clevertap from 'clevertap-web-sdk';
+
+
+clevertap.privacy.push({optOut: false}) // Set the flag to true, if the user of the device opts out of sharing their data
+clevertap.privacy.push({useIP: false})  // Set the flag to true, if the user agrees to share their IP data
+clevertap.init('W44-Z4K-K65Z', 'eu1') 
 
 ReactDOM.render(
   <React.StrictMode>
